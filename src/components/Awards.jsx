45cf88c6cf3425e3012d9ec07417c49839662508
kitmeechai01 <kitmeechai01@gmail.com>
2026-09-24 22:43:30 +0700

    update Awards component

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -7,8 +7,8 @@ const AWARDS = [
   { name: "Diplôme — Inventions Geneva", image: "/assets/Awards/LINE_ALBUM_รางวัล_260924_3.jpg" },
   { name: "NRCT Special Award", image: "/assets/Awards/LINE_ALBUM_รางวัล_260924_2.jpg" },
   { name: "Taiwan Invention Association — Certificate of Achievement", image: "/assets/Awards/LINE_ALBUM_รางวัล_260924_1.jpg" },
-  { name: "Awards 01", image: "public/assets/Awards/LINE_ALBUM_รูปภาพรางวัล_260916_1.png" },
-  { name: "Awards 02", image: "public/assets/Awards/LINE_ALBUM_รูปภาพรางวัล_260916_2.jpg" },
+  { name: "Awards 01", image: "/assets/Awards/LINE_ALBUM_รูปภาพรางวัล_260916_1.png" },
+  { name: "Awards 02", image: "/assets/Awards/LINE_ALBUM_รูปภาพรางวัล_260916_2.jpg" },
 
 ];
 
